6c1d1b3cb735f9c4c82516fe8cb54b754c6ba738
Ibiayi <ibiayi.briggs@gmail.com>
2015-12-13 23:25:46 -0500

    Change toolbar size

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 
 //Create div with the class of a random color and shape.
 var randomColor = function() {
-	color = ['red', 'red', 'red', 'blue', 'blue', 'blue', 'yellow', 'yellow', 'yellow', 'black', 'black', 'grey', 'grey'];
+	color = ['red', 'blue', 'yellow', 'black', 'grey', 'grey'];
 	return color[Math.floor(Math.random()*color.length)];
 }
 
 var randomShape = function() {
-	shape = ['small-square', 'med-square', 'large-square', 'large-square', 'xlarge-square'];
+	shape = ['small-square', 'med-square', 'large-square', 'xlarge-square'];
 	return shape[Math.floor(Math.random()*shape.length)];
 }
 
@@ -15,14 +15,16 @@ function createDiv() {
 	return div;
 }
 
-//Number of shapes to be created.
-
-
 
 //Add divs to document when button clicked.
 $("#randomize").click(function() {
 	$("#art").empty();
 	$("#grid").css("visibility", "visible");
+	$("#art").append(createDiv());
+	$("#art").append(createDiv());
+	$("#art").append(createDiv());
+	$("#art").append(createDiv());
+	$("#art").append(createDiv());
 	$("#art").append(createDiv());
 	$("#art").append(createDiv());
 	$("#art").append(createDiv());
