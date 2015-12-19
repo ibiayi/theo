8a7ef3321e6d0eb7f9018ed2197ffa15506ac343
Ibiayi <ibiayi.briggs@gmail.com>
2015-12-19 12:33:37 -0500

    Added dat.ui toolbar

--- js/app.js
+++ js/app.js
@@ -3,38 +3,51 @@
 var randomColor = function() {
 	color = ['red', 'blue', 'yellow', 'black', 'grey'];
 	return color[Math.floor(Math.random()*color.length)];
-}
+};
 
 var randomShape = function() {
 	shape = ['small-square', 'med-square', 'large-square', 'xlarge-square'];
 	return shape[Math.floor(Math.random()*shape.length)];
-}
+};
 
-function createDiv() {
+var createDiv = function() {
 	var div = '<div class="item '+ randomColor() + ' ' + randomShape() + '"></div>';
 	return div;
-}
+};
+
+//Create x number of divs.
+//var numberOfDivs = parseInt(prompt('How many divs should be created?'));
+
+var divBlock = function(){
+	var i = 0;
+	while (i < Math.round(settings.numberOfDivs)){
+		$("#art").append(createDiv());
+		i = i+1;
+	}
+};
+
+//dat.gui toolbar
+var Settings = function(){
+	this.numberOfDivs = 15
+};
+
+var settings = new Settings();
+
+window.onload=function(){
+	var gui = new dat.GUI({autoPlace: false});
+	gui.add(settings, 'numberOfDivs', 0, 15);
+	$("#gui-container").append(gui.domElement);
+};
+
+
+
+
 
 //Add divs to document when button clicked.
 $("#randomize").click(function() {
 	$("#art").empty();
 	$("#grid").css("visibility", "visible");
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
-	$("#art").append(createDiv());
+	divBlock();
 	//Packery
 	var $container = $('#canvas');
 	$container.packery('destroy');
